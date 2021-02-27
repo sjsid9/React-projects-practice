0d168be4a83ef9414ffbfa4f8c1aadabde6d6eeb
Siddhant <siddhant.jain@quovantis.com>
2021-02-28 00:24:12 +0530

    convert class based component into functional component
    Signed-off-by: Siddhant <siddhant.jain@quovantis.com>

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = (props) => {
+  const [personState, setPersonState] = useState({
     persons: [
       { name: 'Siddhant', age: '24' },
       { name: 'Iron man', age: '55' },
@@ -11,10 +11,14 @@ class App extends Component {
       { name: 'Batman', age: '55' },
       { name: 'Hulk', age: 588 },
     ],
-  };
+  });
 
-  switchButtonHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState('Some other value');
+
+  console.log(personState, otherState);
+
+  const switchButtonHandler = () => {
+    setPersonState({
       persons: [
         { name: 'Siddhant', age: '244' },
         { name: 'Iron man', age: '55' },
@@ -25,43 +29,41 @@ class App extends Component {
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h2>Hi I am a React App</h2>
-        <button onClick={this.switchButtonHandler}>Switch</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        />
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-        <Person
-          name={this.state.persons[3].name}
-          age={this.state.persons[3].age}
-        >
-          {' '}
-          I am a legendary warrior{' '}
-        </Person>
-        <Person
-          name={this.state.persons[4].name}
-          age={this.state.persons[4].age}
-        />
-      </div>
-    );
-    // return React.createElement(
-    //   'div',
-    //   { className: 'App' },
-    //   'Hello',
-    //   React.createElement('h1', null, 'I am a h1 tag')
-    // );
-  }
-}
+  return (
+    <div className="App">
+      <h2>Hi I am a React App</h2>
+      <button onClick={switchButtonHandler}>Switch</button>
+      <Person
+        name={personState.persons[0].name}
+        age={personState.persons[0].age}
+      />
+      <Person
+        name={personState.persons[1].name}
+        age={personState.persons[1].age}
+      />
+      <Person
+        name={personState.persons[2].name}
+        age={personState.persons[2].age}
+      />
+      <Person
+        name={personState.persons[3].name}
+        age={personState.persons[3].age}
+      >
+        {' '}
+        I am a legendary warrior{' '}
+      </Person>
+      <Person
+        name={personState.persons[4].name}
+        age={personState.persons[4].age}
+      />
+    </div>
+  );
+  // return React.createElement(
+  //   'div',
+  //   { className: 'App' },
+  //   'Hello',
+  //   React.createElement('h1', null, 'I am a h1 tag')
+  // );
+};
 
-export default App;
+export default app;
