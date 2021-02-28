eaaada52ce6320b239c1d54f2b6be9b292197f77
Siddhant <siddhant.jain@quovantis.com>
2021-03-01 01:32:30 +0530

    conditional jsx
    Signed-off-by: Siddhant <siddhant.jain@quovantis.com>

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -13,21 +13,23 @@ const app = (props) => {
     ],
   });
 
-  const [otherState, setOtherState] = useState('Some other value');
+  // const [otherState, setOtherState] = useState('Some other value');
 
-  console.log(personState, otherState);
+  // console.log(personState, otherState);
 
-  const switchButtonHandler = () => {
-    setPersonState({
-      persons: [
-        { name: 'Siddhant', age: '244' },
-        { name: 'Iron man', age: '55' },
-        { name: 'Steve', age: '55' },
-        { name: 'Batman', age: '55' },
-        { name: 'Hulk', age: 58 },
-      ],
-    });
-  };
+  // const switchButtonHandler = () => {
+  //   setPersonState({
+  //     persons: [
+  //       { name: 'Siddhant', age: '244' },
+  //       { name: 'Iron man', age: '55' },
+  //       { name: 'Steve', age: '55' },
+  //       { name: 'Batman', age: '55' },
+  //       { name: 'Hulk', age: 58 },
+  //     ],
+  //   });
+  // };
+
+  const [showPersonsState, setShowPersonsState] = useState(false);
 
   const setNameViaInput = (event) => {
     setPersonState({
@@ -39,6 +41,10 @@ const app = (props) => {
         { name: 'Hulk', age: 58 },
       ],
     });
+  };
+
+  const togglePersons = () => {
+    setShowPersonsState(!showPersonsState);
   };
 
   const nameChangeHandler = (newName) => {
@@ -56,32 +62,36 @@ const app = (props) => {
   return (
     <div className="App">
       <h2>Hi I am a React App</h2>
-      <button onClick={() => nameChangeHandler('Bucky Barnes')}>Switch</button>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}
-        inputChange={setNameViaInput}
-      />
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}
-        click={nameChangeHandler.bind(null, 'Captain America')}
-      />
-      <Person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}
-      />
-      <Person
-        name={personState.persons[3].name}
-        age={personState.persons[3].age}
-      >
-        {' '}
-        I am a legendary warrior{' '}
-      </Person>
-      <Person
-        name={personState.persons[4].name}
-        age={personState.persons[4].age}
-      />
+      <button onClick={() => togglePersons()}>Switch</button>
+      {showPersonsState ? (
+        <div>
+          <Person
+            name={personState.persons[0].name}
+            age={personState.persons[0].age}
+            inputChange={setNameViaInput}
+          />
+          <Person
+            name={personState.persons[1].name}
+            age={personState.persons[1].age}
+            click={nameChangeHandler.bind(null, 'Captain America')}
+          />
+          <Person
+            name={personState.persons[2].name}
+            age={personState.persons[2].age}
+          />
+          <Person
+            name={personState.persons[3].name}
+            age={personState.persons[3].age}
+          >
+            {' '}
+            I am a legendary warrior{' '}
+          </Person>
+          <Person
+            name={personState.persons[4].name}
+            age={personState.persons[4].age}
+          />
+        </div>
+      ) : null}
     </div>
   );
   // return React.createElement(
